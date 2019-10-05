a3a563cd5ddfc0fb6863a0daf62f354bc45daf47
Alexander Goncharuk <sashael@ukr.net>
2019-10-06 01:18:49 +0300

    ready

---
+++ cs3/Group.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs3
+{
+    interface ISortSt
+    {
+        void Sort(List<Student> students);
+    }
+    class SortRecBook : ISortSt
+    {
+        public void Sort(List<Student> students)
+        {
+            students.Sort((x, y) => x.RecordBook.CompareTo(y.RecordBook));
+        }
+    }
+    class SortSurname : ISortSt
+    {
+        public void Sort(List<Student> students)
+        {
+            students.Sort((x, y) => x.Surname.CompareTo(y.Surname));
+        }
+    }
+    class SortAverage : ISortSt
+    {
+        public void Sort(List<Student> students)
+        {
+            students.Sort((x, y) => y.Average().CompareTo(x.Average()));
+        }
+    }
+    class Group
+    {
+        Random rnd = new Random();
+
+        List<Student> students = new List<Student>();
+        string name;
+        private ISortSt strategy;
+        public Group(string name = "noname group") { this.name = name; }
+        public void AddSt(Student st)
+        {
+            st.Group = name;
+            students.Add(st);
+        }
+        public void RemSt(string surname)
+        {
+            students.Remove(students.Find(st => st.Surname == surname));
+        }
+        public void RemSt(int recordbook)
+        {
+            students.Remove(students.Find(st => st.RecordBook == recordbook));
+        }
+        public void Show()
+        {
+            foreach (var el in students)
+                el.Info();
+        }
+        public void SetStrategy(ISortSt strat)
+        {
+            if (strat != null)
+                strategy = strat;
+        }
+        public void SortGr()
+        {
+            strategy.Sort(students);
+        }
+        public void SetRandMarks()
+        {
+            uint[] arr;
+            foreach (var el in students)
+            {
+                arr = new uint[rnd.Next(1, 10)];
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    arr[j] = Convert.ToUInt32(rnd.Next(1, 13));
+                }
+                el.SetAdm(arr);
+                arr = new uint[rnd.Next(1, 10)];
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    arr[j] = Convert.ToUInt32(rnd.Next(1, 13));
+                }
+                el.SetDes(arr);
+                arr = new uint[rnd.Next(1, 10)];
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    arr[j] = Convert.ToUInt32(rnd.Next(1, 13));
+                }
+                el.SetProg(arr);
+            }
+        }
+        public void ShowBest(int number = 3)
+        {
+            if (number <= students.Count && number > 0)
+            {
+                List<Student> st_copy = new List<Student>(students);
+                var tmp = new SortAverage();
+                tmp.Sort(st_copy);
+                Console.WriteLine($"Best {number} students:");
+                for (int i = 0; i < st_copy.Count && i < number; i++)
+                {
+                    st_copy[i].Info();
+                }
+            }
+            else
+                Console.WriteLine("Invalid index.");
+        }
+    }
+}
---
+++ cs3/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs3
+{
+    class Program
+    {
+        static void Task1()
+        {
+            Random rnd = new Random();
+            List<Student> students = new List<Student>();
+            students.Add(new Student("name1", "surn1", "patr1"));
+            students.Add(new Student("name2", "surn2", "patr2"));
+            students.Add(new Student("name3", "surn3", "patr3"));
+            students.Add(new Student("name4", "surn4", "patr4"));
+            students.Add(new Student("name5", "surn5", "patr5"));
+            uint[] arr;
+            foreach (var el in students)
+            {
+                arr = new uint[rnd.Next(1, 10)];
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    arr[j] = Convert.ToUInt32(rnd.Next(1, 13));
+                }
+                el.SetAdm(arr);
+                arr = new uint[rnd.Next(1, 10)];
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    arr[j] = Convert.ToUInt32(rnd.Next(1, 13));
+                }
+                el.SetDes(arr);
+                arr = new uint[rnd.Next(1, 10)];
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    arr[j] = Convert.ToUInt32(rnd.Next(1, 13));
+                }
+                el.SetProg(arr);
+            }
+            foreach (var el in students)
+                el.Info();
+            Console.WriteLine("Student with max average mark:");
+            Student.MaxAv(students);
+            Console.WriteLine("Student with min average mark:");
+            Student.MinAv(students);
+            Student.PassedSubject(students);
+        }
+        static void Task2()
+        {
+            Group group = new Group("33СПА-2");
+            group.AddSt(new Student("name_one", "surn_one", "patr_one"));
+            group.AddSt(new Student("name_two", "surn_two", "patr_two"));
+            group.AddSt(new Student("name_three", "surn_three", "patr_three"));
+            group.AddSt(new Student("name_four", "surn_four", "patr_four"));
+            group.AddSt(new Student("name_five", "surn_five", "patr_five"));
+            group.AddSt(new Student("name_six", "surn_six", "patr_six"));
+            group.SetRandMarks();
+            group.Show();
+
+            Console.WriteLine("remove student six===============================");
+            group.RemSt("surnsix");
+            group.Show();
+
+            Console.WriteLine("Sort by surname===============================");
+            ISortSt strategy = new SortSurname();
+            group.SetStrategy(strategy);
+            group.SortGr();
+            group.Show();
+
+            Console.WriteLine("Sort by average===============================");
+            strategy = new SortAverage();
+            group.SetStrategy(strategy);
+            group.SortGr();
+            group.Show();
+
+            Console.WriteLine("Sort by recbook===============================");
+            strategy = new SortRecBook();
+            group.SetStrategy(strategy);
+            group.SortGr();
+            group.Show();
+
+            group.ShowBest();
+        }
+        static void Main(string[] args)
+        {
+            Task1();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
+            Task2();
+        }
+    }
+}
